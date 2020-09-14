33a6fab16dbc8c27c2a00db5f96dcc91a2877a25
raghu <thesoul7>
2020-09-14 13:10:18 +0530

    first commit

---
+++ src/test/java/com/testscripts/OneScript.java
@@ -0,0 +1,12 @@
+package com.testscripts;
+
+import org.testng.annotations.Test;
+
+import com.generic.BaseClass;
+
+public class OneScript extends BaseClass {
+@Test
+public void scriptOne() {
+	System.out.println("Hello");
+}
+}
